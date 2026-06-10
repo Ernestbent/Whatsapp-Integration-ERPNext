b81f4fe5cef4621860ddb7194504ebd60361f886
Ernestbent <ernestben69@gmail.com>
2026-06-10 12:21:52 +0300

    fix:Removed Notifications On Sales Order Submission

--- whatsapp_integration/hooks.py
+++ whatsapp_integration/hooks.py
@@ -160,10 +160,10 @@ doctype_js = {
 # Hook on document methods and events
 doc_events = {
     "Sales Order": {
-        "on_update_after_submit": [
-            "whatsapp_integration.erpnext_whatsapp.custom_scripts.send_message_template_sales_order.on_sales_order_workflow_change",
-            "whatsapp_integration.erpnext_whatsapp.custom_scripts.send_message_template_in_transit.on_sales_order_in_transit"
-        ],
+        # "on_update_after_submit": [
+        #     "whatsapp_integration.erpnext_whatsapp.custom_scripts.send_message_template_sales_order.on_sales_order_workflow_change",
+        #     "whatsapp_integration.erpnext_whatsapp.custom_scripts.send_message_template_in_transit.on_sales_order_in_transit"
+        # ],
     },
     # "Sales Invoice": {
     #     "on_submit": [
