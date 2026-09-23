a509c5e49f6d0d381c97636ec2d755075eb8215d
Ernestbent <ernestben69@gmail.com>
2026-09-23 09:13:11 +0300

    Pushing to Production

---
+++ whatsapp_integration/erpnext_whatsapp/doctype/stock_alert_settings/stock_alert_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Autozone Professional Limited and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Stock Alert Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });
